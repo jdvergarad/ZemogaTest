915be02e8c17232bde065832a720048dfc7484c4
jdvergarad <jdvergarad@hotmail.com>
2021-08-17 16:31:44 -0500

    Adding basic functionalities to post component

--- ZemogaTest/ClientApp/src/app/post/post.component.ts
+++ ZemogaTest/ClientApp/src/app/post/post.component.ts
@@ -10,35 +10,37 @@ import { comment } from '../models/comment';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  public post: post;
+  public post = new post();
   public postId: string;
   public role: string;
   public username: string;
+  public postStatus = '';
+  public decisionMessage = '';
+  public canEdit = false;
   public comment = new comment();
 
   constructor(protected _postService: PostServiceService, private activatedRoute: ActivatedRoute) {
     this.postId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.role = JSON.parse(localStorage.getItem('user')).role;
-    this.username = JSON.parse(localStorage.getItem('user')).username;
+    var logedUser = JSON.parse(localStorage.getItem('user'));
+    this.role = logedUser.role;
+    this.username = logedUser.username;
+  }
+  
+  async ngOnInit() {
     this.comment.content = '';
-  }
+    this.post = await this._postService.GetPostById(this.postId).toPromise();
+    this.postStatus = this.post.status;
 
-  ngOnInit() {
-    this.GetPost(this.postId);
-  }
-
-  async GetPost(postId: string){
-    this.post = await this._postService.GetPostById(postId).toPromise();
-    console.log(this.post);
+    if(this.post.authorUsername.toString() == this.username.toString()
+        && this.post.status != "Published") {
+      this.canEdit = true;
+    }
   }
 
   async AddComment(){
     this.comment.postId = this.postId;
     this.comment.authorUsername = this.username;
-
-    var postCommented = await this._postService.AddComment(this.comment).toPromise();
-
-    console.log(postCommented);
+    await this._postService.AddComment(this.comment).toPromise();
     window.location.reload();
   }
 }
